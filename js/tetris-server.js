cc47de4bd05c0d4a7105fe9b2ccb473e7fd54780
Daniel Ruiz de Alegría <daniel@drasite.com>
2020-03-04 11:02:49 +0100

    Configure node project

--- js/tetris-server.js
+++ js/tetris-server.js
@@ -6,13 +6,14 @@ const WebSocket = require('ws')
 const fs = require('fs')
 const TetrisServer = require('./Tetris/TetrisServer')
 
-const scoreListFile = '../score-list.json'
+const [PORT] = process.argv.slice(2)
+const SCORELIST_FILE = '../score-list.json'
 
 process.on('uncaughtException', function (err) {
   console.error('Caught exception: ', err)
 })
 
-const webSocketServer = new WebSocket.Server({ port: 8080 })
+const webSocketServer = new WebSocket.Server({ port: PORT })
 webSocketServer.on('connection', webSocket => {
-  new TetrisServer(webSocket, scoreListFile)
+  new TetrisServer(webSocket, SCORELIST_FILE)
 })
